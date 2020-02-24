53155e72507d2228db316fef3ddae234f64e4963
dwkielman <dwkielman@gmail.com>
2020-02-23 21:54:32 -0800

    Build path configured

--- cs535-PA2/src/main/java/cs535/drivers/TestClass.java
+++ cs535-PA2/src/main/java/cs535/drivers/TestClass.java
@@ -1,5 +1,15 @@
 package cs535.drivers;
 
+import org.apache.storm.spout.SpoutOutputCollector;
+
+import twitter4j.TwitterStream;
+
 public class TestClass {
+	
+	SpoutOutputCollector collector;
+	
+	public static void main(String[] args) {
+        TwitterStream ts;
+    }
 
 }
